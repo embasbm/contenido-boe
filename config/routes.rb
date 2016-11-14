4a4d73d2f741c0d508a7235d4122add48ee420c5
Emba Moussa <emba.moussa@airtimerewards.co>
2016-11-14 16:54:35 +0000

    add sidekiq web route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(username, ENV["sidekiq_username"]) &
+      ActiveSupport::SecurityUtils.secure_compare(password, ENV["sidekiq_password"])
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
+
   resources :diaries, only: [:index]
   root to: 'diaries#index'
   get '*path' => redirect('/')
