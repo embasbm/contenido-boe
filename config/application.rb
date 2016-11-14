6efaad65e6fc4ac30808c662844df01cb38fbc83
Emba Moussa <emba.moussa@airtimerewards.co>
2016-11-14 22:17:20 +0000

    load workers folder

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module ContenidoBoe
   # ContenidoBoe Application class
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/app/workers)
     config.active_job.queue_adapter = :sidekiq
   end
 end
