2e8e9f03106464b164fc9d6899ad57187d24371d
Emba Moussa <emba.moussa@airtimerewards.co>
2016-11-14 23:23:13 +0000

    slowing down sidekiq pid files and concurrency

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ role :web, %w(deploy@contenido-boe.es)
 role :db,  %w(deploy@contenido-boe.es)
 
 set :sidekiq_env,           'production'
-set :sidekiq_processes,     2
+set :sidekiq_processes,     1
 set :sidekiq_config, -> { File.join(current_path, 'config', 'sidekiq.yml') }
 set :sidekiq_pid, -> { File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid') }
 set :sidekiq_log, -> { File.join(shared_path, 'log', 'sidekiq.log') }
