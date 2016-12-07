9a85855d8a2109bb013b17a9645b86479e6f1f6b
Emba Moussa <emba.moussa@airtimerewards.co>
2016-12-07 00:14:56 +0000

    add missing secret for ails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# rubocop:disable Metrics/LineLength
+
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContenidoBoe::Application.config.secret_token = 'bea5bc901d49e083256e2257f9ae0507565ae702693c668d433811aea620828b69bc8a943ddbe661bbc99a03ea2b906c523eb0593a38273ee343e20354501e97'
